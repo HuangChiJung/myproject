23b2c39e269eb926f54fba189b772f21f24aba60
Ifrit <ifhuangif@gmail.com>
2016-01-04 20:43:13 +0800

    字串的比較

---
+++ src/com/tom/Wizard.java
@@ -0,0 +1,45 @@
+package com.tom;
+
+ 
+
+public class Wizard  extends Player {
+
+	@Override
+	public void attack() {
+		System.out.println("使出定身術，周圍無法動彈三回合");	
+	}
+	
+	public static void main(String[] args) {
+		Integer i = new Integer(100);
+		
+		int n = i;
+		int n2 = i.intValue();
+		
+		Integer i2 = 100;
+		
+		
+//		String s1 = new String("abc");
+//		
+//		String s2 = new String("abc");
+		
+		
+		String s1 = "abc";  //編譯器 雞婆  認為 S1 及 S2  是同一個常數, 就創建一份
+		
+		String s2 = "abc";
+		
+		
+		
+		System.out.println(s1==s2);
+		System.out.println(s1.equals(s2));
+		
+		
+		
+		StringBuilder sb = new StringBuilder("abc");
+		StringBuffer sb2 = new StringBuffer(); //多執行續
+		
+		sb.append("xyz");
+		
+		System.out.println(sb);
+		
+	}
+}
