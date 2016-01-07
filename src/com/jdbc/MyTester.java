4e080c21d5966c194c1acceab1155b32b6fda7c6
Ifrit <ifhuangif@gmail.com>
2016-01-07 19:35:03 +0800

    sql

--- src/com/jdbc/MyTester.java
+++ src/com/jdbc/MyTester.java
@@ -2,15 +2,30 @@ package com.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
 
 public class MyTester {
 
 	public static void main(String[] args) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			String url = "jdbc:mysql://j.snpy.org/java1?user=java1&password=jjaa989";//j.snpy.org     java1  jjaa989     java1
+			String url = "jdbc:mysql://j.snpy.org/java1?user=java1&password=jjaa989&characterEncoding=UTF-8&useUnicode=true";//j.snpy.org     java1  jjaa989     java1
 			Connection conn = DriverManager.getConnection(url);
+			Statement stmt =  conn.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from parking");
+			
+			while(rs.next()){
+				String cid = rs.getString("car_id");
+				Date date = rs.getDate("ctime");
+				int type = rs.getInt("type");
+				
+				System.out.println(cid+"\t"+date+"\t"+type);
+			}
+			
+			
 		} catch (InstantiationException | IllegalAccessException | ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
